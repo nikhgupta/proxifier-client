c1eb5fc8cb4403a5f118874fa0407e53e3234eb1
Nikhil Gupta <me@nikhgupta.com>
2016-10-16 10:25:32 +0530

    removed jquery-ui from JS, ignored vendor specific assets from build; added asset hasing

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+ignore "pages-framework/*"
 activate :sprockets
 
 configure :development do
@@ -11,4 +12,6 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
+  activate :asset_hash
+  activate :gzip
 end
